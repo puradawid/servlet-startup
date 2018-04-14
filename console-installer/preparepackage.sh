767bf72b0a8353d9da2069e63af0a6953ed2ec1e
Dawid Pura <puradawid@gmail.com>
2018-04-14 13:00:36 +0200

    Make build script more verbose

--- console-installer/preparepackage.sh
+++ console-installer/preparepackage.sh
@@ -1,5 +1,12 @@
+mvn clean package assembly:single
+echo "Removing dist folder"
 rm -rf dist
+echo "Creating dist folder"
 mkdir dist
+echo "Copy $(ls target | grep jar-with-dependencies) to dist"
 cp target/console-installer-*-jar-with-dependencies.jar dist/console-installer.jar
+echo "Copy run script"
 cp src/script/run.sh dist/run
 chmod +x dist/run
+
+echo "Done"
